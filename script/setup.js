b606f6635d45b5d7ae352eeeef9885fea3a02481
Bo <boryslav.kurinnyi@gmail.com>
2021-05-08 20:04:21 +0300

    2 commit

--- script/setup.js
+++ script/setup.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
   $('.my-carousel').slick({
-    'setting-name': 'setting-value'
+    adaptiveHeight: true,
+    slidesToShow: 3, //количество товаров в линии
+    autoplay: true, //автопереключение
+    autoplaySpeed: 1000, //скорость автопереключения(мс)
+    prevArrow: '<button type="button" class="my-carousel-prev">Previous</button>',//кнопка назад
+    nextArrow: '<button type="button" class="my-carousel-next">Next</button>'//кнопка вперёд
   });
 });
